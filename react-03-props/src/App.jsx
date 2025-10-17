d4c6023cfbb5c42dbd2f5fd6968bc37630205276
ShivamChowbey <shivamchowbey.aiml2024@nsec.ac.in>
2025-10-17 09:03:28 +0000

    react-03

--- react-03-props/src/App.jsx
+++ react-03-props/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <div className='parent'>
-      <div className="card">
-        <h1>Shivam Chowbey</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, maiores.</p>
-        <button>View</button>
-      </div>
+     
+      <Card user ="Shivam Chowbey" img="https://images.unsplash.com/photo-1759399703184-11a722ec0706?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764"/>
+      <Card user="Sangita Das" img = "https://plus.unsplash.com/premium_photo-1759937765271-2ac02feb860b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=689"/>
     </div>
   )
 }
