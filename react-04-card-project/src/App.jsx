3e4e93fa8f2d246640f34b82107e7d69eae58855
ShivamChowbey <shivamchowbey.aiml2024@nsec.ac.in>
2025-10-17 09:34:11 +0000

    04-card-project

---
+++ react-04-card-project/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BookmarkCheck} from 'lucide-react'
+const App = () => {
+  return (
+    <div className='parent'>
+
+      <div className="card">
+        <div className="top">
+          <img src="https://www.tripfiction.com/wp-content/uploads/2016/08/1920x1080-brands-amazon-logo.jpg" alt="" />
+          <button>Save <BookmarkCheck /> </button>
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="bottom">
+
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
