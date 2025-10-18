f6cc9ab2c0aab81bbd56a38d7377bff4dcd6d265
ShivamChowbey <shivamchowbey.aiml2024@nsec.ac.in>
2025-10-18 08:40:47 +0000

    05-css

---
+++ react-05-css/src/components/Button.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import '../styles/Button.css'
+const Button = () => {
+  return (
+    <div>
+       <div className='btn'>Mai hu ek button</div>
+    </div>
+  )
+}
+
+export default Button
---
+++ react-05-css/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import '../styles/Header.css'
+const Header = () => {
+  return (
+    <div>
+        <div className='headers'>Shivam Chowbey</div>
+    </div>
+  )
+}
+
+export default Header
