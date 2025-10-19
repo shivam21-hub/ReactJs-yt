fded94a3a1e1396924c6a5e7e8f21bd59116fd11
ShivamChowbey <shivamchowbey.aiml2024@nsec.ac.in>
2025-10-19 13:17:38 +0000

    07-function

---
+++ react-07-function/src/App.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+const App = () => {
+
+    // function ClickHandler(){
+    //     console.log("love");
+        
+    // }
+  return (
+    <div>
+      {/* <h1>Shivam Chowbey</h1>
+      <button onClick={()=> {
+        console.log("Money");
+        
+      }}>Click me</button> */}
+
+      {/* <input type="text" placeholder='Enter your name'  onChange={(val)=>{
+        console.log(val.target.value);
+        
+      }}/> */}
+
+      
+    </div>
+  )
+}
+
+export default App
