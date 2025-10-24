52ebf79e5965ec7e627a497dd1fa6f6ac58ce9ba
ShivamChowbey <shivamchowbey.aiml2024@nsec.ac.in>
2025-10-24 16:04:14 +0000

    16-Router Dom

---
+++ react-16-router-dom/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Route } from 'lucide-react'
+import React from 'react'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element = {<About/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
